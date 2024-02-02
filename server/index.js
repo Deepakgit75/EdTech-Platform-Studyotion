57b5d1234da0e86a92a60207ac3ace0038920f2d
Deepak Malviya <107317546+Deepakgit75@users.noreply.github.com>
2024-02-02 21:28:49 +0530

    Update index.js cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"'https://studymotion-backend.onrender.com",
 		credentials:true,
 	})
 )
