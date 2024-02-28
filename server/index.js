f059d5683086ec375f6a91c02f16459f22c5a69b
Deepak Malviya <107317546+Deepakgit75@users.noreply.github.com>
2024-02-28 09:14:50 +0530

    Update index.js /

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studymotion-frontend.vercel.app/",
+		origin:"https://studymotion-frontend.vercel.app",
 		credentials:true,
 	})
 )
